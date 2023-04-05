96cfd78ccd662c794a805158959fe9fc5cbea6d2
arasaa <arazsaydu@gmail.com>
2023-04-06 01:27:44 +0200

    created partFive an audioplayer component

---
+++ src/deutsch/parts/PartFiveAudio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PartFiveAudio = () => {
+  return (
+    <div>
+    <h2>Audio</h2>
+    <audio src="example.mp3" controls />
+  </div>
+  )
+}
+
+export default PartFiveAudio
